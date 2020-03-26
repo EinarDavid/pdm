c80db24fc03dcabe1b99f4d2c54530456ac420b3
Einar David <einardavidvillarroel@gmail.com>
2020-03-25 20:33:39 -0400

    Clase donde se hizo el Boton

--- MyfirstAppRN/App.js
+++ MyfirstAppRN/App.js
@@ -27,29 +27,41 @@ import{Colors} from 'react-native/Libraries/NewAppScreen';
 import MyComponent from'./src/test/MyComponent';
 import Logo from './src/login/logo';
 import Input from './src/login/input';
+import Boton from './src/login/button';
 import imgPassword from './src/images/pass.png';
 import imgUser from './src/images/username.png';
 import Constans from './src/login/constans';
+
+const PASS='123';
+const USER='user1';
+const NUM1 =4;
+const NUM2 = 2;
 
 const App: () => React$Node = () => {
   return (
     <View style={styles.body}>
       <View style={styles.container}>
-      <MyComponent style={styles.box} text="Hola Mundo Desde React"></MyComponent>
-      <MyComponent style={styles.box} text="ED"></MyComponent>
+      
       <Logo style={styles.box}></Logo>
+      
       <Input
-      source={imgPassword}
+      source={imgUser}
       placeholder={Constans.USER}
       secureTextEntry={false}
       autoCorrect={false}
+      onChangeText={USER => this.setState({USER})}
+      onChangeText={NUM1 => this.setState({NUM1})}
       />
       <Input
-      source={imgUser}
+      source={imgPassword}
       placeholder={Constans.PASS}
       secureTextEntry={true}
       autoCorrect={false}
+      onChangeText={PASS => this.setState({PASS})}
+      onChangeText={NUM2 => this.setState({NUM2})}
       />
+      <Boton pass={PASS} username={USER}
+      num1={NUM1} num2={NUM2}></Boton>
       </View>
       
     </View>
@@ -62,7 +74,8 @@ const styles = StyleSheet.create({
   body:{
     flex:1,
     flexDirection:'column',
-    backgroundColor: Colors.primary,
+    backgroundColor: '#1292B4',
+  
   },
   container:{
     flex: .5,
@@ -76,6 +89,8 @@ const styles = StyleSheet.create({
 
 export default App;
 /*
+
+
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
